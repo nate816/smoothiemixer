4dace80b135b2a6d608fbd53ac39fa1706802585
AnoplexAdmin <nate@anoplex.com>
2021-07-30 10:44:16 -0700

    changed red to light green

--- src/img/index.js
+++ src/img/index.js
@@ -1,3 +1,3 @@
-export const img1 = require('./smoothie-1.svg');
+export const img1 = require('./smoothie-7.svg');
 export const img2 = require('./smoothie-2.svg');
 export const img3 = require('./smoothie-8.svg');
